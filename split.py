5f9d74eed98052c28a32979141089e783e2da50d
Michal Kolář <michalkolar27@gmail.com>
2019-12-21 23:01:27 +0100

    commentary added

--- split.py
+++ split.py
@@ -13,6 +13,12 @@ for feat in features:
 
 # quadtree
 features_new = quadtree.quadtree(features)
+
+# quadtree_turtle(features, resolution, line, dot_size)
+# is possible to change 2nd to 4th argument
+# resolution = side of a rescaling scale (recommended values: 300-1000), depends on points length
+# line = size of bounding box line, is reduced by half with every recursion
+# dot_size = size of dot symbolizing address point (recommended values: 3-10), depends on points length and resolution
 quadtree.quadtree_turtle(features, 700, 10, 5)
 
 # export preparation
