086d62ec649c77522600295f60952b900e804e03
Michal Kolář <michalkolar27@gmail.com>
2019-12-21 22:29:01 +0100

    quadtree algorithm correction

--- quadtree.py
+++ quadtree.py
@@ -4,31 +4,23 @@ from random import randint
 # new list "final_features"
 final_features = []
 
-# function that returns x and y mid values
-def quadtree_mids(features):
-    #new list of coordinates
-    points = []
-    for feat in features:
-        coordinates = feat["geometry"]["coordinates"]
-        points.append(coordinates)
+
+# function that returns x and y max/min values
+def quadtree_bbox(points):
     # getting lists of x and y coordinates
     points_x = [pointx[0] for pointx in points]
     points_y = [pointy[1] for pointy in points]
-    # calculation of mids
+    # calculation of max/min
     x_min = min(points_x)
     x_max = max(points_x)
     y_min = min(points_y)
     y_max = max(points_y)
-    x_mid = (x_max + x_min) / 2
-    y_mid = (y_max + y_min) / 2
-    return x_mid, y_mid
+    return x_min, x_max, y_min, y_max
 
 
-# quadtree main function
-def quadtree(features):
+def quadtree_clustering(features, x_min, x_max, y_min, y_max):
     print("Points count: ", len(features))
 
-    # setting up 4 new lists
     NW = []  # north-west quadrant
     NE = []  # north-east quadrant
     SW = []  # south-west quadrant
@@ -36,7 +28,8 @@ def quadtree(features):
 
     # if list has less than 50 points, goes to "else" and append points with new "cluster_ID" number to "final_features"
     if len(features) > 50:
-        x_mid, y_mid = quadtree_mids(features)
+        x_mid = (x_max + x_min) / 2
+        y_mid = (y_max + y_min) / 2
         # appending points with new cluster ids (based on X/Y values compared to mid values) to 4 quadrants NW/NE/SW/SE
         for feat in features:
             x, y = feat["geometry"]["coordinates"]
@@ -59,18 +52,28 @@ def quadtree(features):
         print("NE: ", NE)
 
         # recursion of the function with new 4 quadrant lists as attributes
-        quadtree(NW)
-        quadtree(NE)
-        quadtree(SW)
-        quadtree(SE)
+        quadtree_clustering(NW, x_min, x_mid, y_mid, y_max)
+        quadtree_clustering(NE, x_mid, x_max, y_mid, y_max)
+        quadtree_clustering(SW, x_min, x_mid, y_min, y_mid)
+        quadtree_clustering(SE, x_mid, x_max, y_min, y_mid)
     else:
         for feat in features:
             final_features.append(feat)
-    print(final_features)
     return final_features
 
 
-def quadtree_turtle_points(xpoints, ypoints, line, dot_size):
+# quadtree main function
+def quadtree(features):
+    points = []
+    for feat in features:
+        coordinates = feat["geometry"]["coordinates"]
+        points.append(coordinates)
+
+    x_min, x_max, y_min, y_max = quadtree_bbox(points)
+
+    return quadtree_clustering(features, x_min, x_max, y_min, y_max)
+
+def quadtree_turtle_points(xpoints, ypoints, dot_size):
     tracer(len(xpoints)*6)
     colormode(255)
     randcolor = (randint(0, 255), randint(0, 225), randint(0, 255))
@@ -82,13 +85,12 @@ def quadtree_turtle_points(xpoints, ypoints, line, dot_size):
         dot(dot_size)
         penup()
 
+
 def quadtree_turtle_draw(points, x_min, x_max, y_min, y_max, line, dot_size):
     xpoints = [pointx[0] for pointx in points]
     ypoints = [pointy[1] for pointy in points]
 
-    quadtree_turtle_points(xpoints, ypoints, line, dot_size)
-
-
+    quadtree_turtle_points(xpoints, ypoints, dot_size)
 
     NW = []  # north-west quadrant
     NE = []  # north-east quadrant
@@ -98,6 +100,7 @@ def quadtree_turtle_draw(points, x_min, x_max, y_min, y_max, line, dot_size):
     x_mid = (x_max + x_min) / 2
     y_mid = (y_max + y_min) / 2
     line = line / 2
+
     if len(points) > 100:
         # appending points with new cluster ids (based on X/Y values compared to mid values) to 4 quadrants NW/NE/SW/SE
         for pnt in points:
@@ -133,24 +136,26 @@ def quadtree_turtle_draw(points, x_min, x_max, y_min, y_max, line, dot_size):
 
 def quadtree_turtle(features, resolution, line, dot_size):
     screensize(resolution+100, resolution+100)
-    speed(10)
+
     points = []
     points_x = []
     points_y = []
     points_scaled = []
     points_x_scaled = []
     points_y_scaled = []
-    x_mid, y_mid = quadtree_mids(features)
+
     for feat in features:
         coordinates = feat["geometry"]["coordinates"]
         points.append(coordinates)
         points_x.append(coordinates[0])
         points_y.append(coordinates[1])
-    x_max = max(points_x) - x_mid
-    y_max = max(points_y) - y_mid
+
+    x_mid = (max(points_x) + min(points_x)) / 2
+    y_mid = (max(points_y) + min(points_y)) / 2
+
     for pnt in points:
-        pointx = (resolution/2)*(pnt[0] - x_mid) / x_max
-        pointy = (resolution/2)*(pnt[1] - y_mid) / y_max
+        pointx = (resolution/2)*(pnt[0] - x_mid) / (max(points_x) - x_mid)
+        pointy = (resolution/2)*(pnt[1] - y_mid) / (max(points_y) - y_mid)
         coordinates_scaled = (pointx, pointy)
         points_scaled.append(coordinates_scaled)
         points_x_scaled.append(coordinates_scaled[0])
